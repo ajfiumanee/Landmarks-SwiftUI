99328beab65c9e00293dfb3367bae5d764c1e0a0
Alberto Júnior <ajfiumanee@outlook.com>
2023-05-03 23:23:40 -0300

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  Landmarks_SwiftUIApp.swift
+//  Landmarks-SwiftUI
+//
+//  Created by Alberto Oliveira Jr on 03/05/23.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
